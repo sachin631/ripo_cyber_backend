24bb5e7a6036abc16c35f6a3c6b3cf0d52171ab3
sachin631 <sangwansachin631@gmail.com>
2025-02-01 01:16:47 +0530

    update

--- src/middlewares/cloudnary.ts
+++ src/middlewares/cloudnary.ts
@@ -2,7 +2,7 @@ import { showResponse } from "../utils/response.utils";
 
 import { v2 as cloudinary } from "cloudinary";
 // const cloudinary = require("cloudinary").v2;
-const fs=require("fs");
+const fs = require("fs");
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -10,23 +10,25 @@ cloudinary.config({
   api_secret: process.env.API_SECRET,
 });
 
-const cloudinaryUploaderr =async (filePath:any)=> {
+const cloudinaryUploaderr = async (filePath: any) => {
   try {
     if (!filePath) {
-        return showResponse(false,'FILE PATH NOT FOUND',null,400);
+      return showResponse(false, 'FILE PATH NOT FOUND', null, 400);
     }
     const cloudRes = await cloudinary.uploader.upload(filePath, {
       resource_type: "auto",
     });
     //delete file from server after uploded on cloudinary
-    fs.unlinkSync(filePath);
-    
+    if (fs.existsSync(filePath)) {
+      fs.unlinkSync(filePath);
+    }
+
     return cloudRes;
 
-  } catch (error:any) {
+  } catch (error: any) {
     //delete the file from server if there is any error while uploding on cloudinary
     fs.unlinkSync(filePath);
-    return showResponse(false,'file not upload on cloudinary please try again',error.message,500)
+    return showResponse(false, 'file not upload on cloudinary please try again', error.message, 500)
   }
 }
 
